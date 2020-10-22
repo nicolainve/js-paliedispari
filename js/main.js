68fd01bd9368d1056defb69ca332ab54cef0e74a
Nicola <nginvernizzi1@gmail.com>
2020-10-22 14:41:48 +0200

    js palindroma done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
-console.log('test');
+// PALINDROMA
+
+var userWord = prompt('Inserisci una parola');
+console.log('Parola: ', userWord);
+
+var reverseUserWord = reverse(userWord);
+
+if (userWord != reverseUserWord) {
+    console.log('Inverso: ', reverseUserWord);
+} else {
+    console.log('Hai inserito una parola palindroma');
+}
+
+function reverse(word) {
+    var wordReverse = "";
+
+    for (var i = word.length - 1; i >= 0; i--) {
+        wordReverse += word[i];   
+    }
+    
+    return wordReverse;
+}
