73a9150f158e852cdef621ddb0b628f5049dffb0
Nicola <nginvernizzi1@gmail.com>
2020-10-22 16:37:59 +0200

    js pari e dispari done

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 // PALINDROMA
 
-var userWord = prompt('Inserisci una parola');
+do {
+    var userWord = prompt('Inserisci una parola');
+} while (userWord == '');
+
 console.log('Parola: ', userWord);
 
 var reverseUserWord = reverse(userWord);
@@ -23,4 +26,54 @@ function reverse(word) {
     */
     
     return reverse;
+}
+
+// PARI E DISPARI
+
+
+do {
+    var choice = prompt('Pari o Dispari').trim();
+} while (choice != 'pari' && choice != 'dispari');
+
+console.log(choice);
+
+do {
+    var userNumber = parseInt( prompt('Inserisci un numero da 1 a 5') );
+} while (userNumber < 1 || userNumber > 5);
+
+console.log(userNumber);
+
+var computerNumber = randomNumber();
+
+console.log(computerNumber);
+
+var sum = userNumber + computerNumber;
+
+console.log(sum);
+
+var result = evenOdd(userNumber, computerNumber);
+
+if (choice == result) {
+    console.log('Hai vinto!');
+} else {
+    console.log('Hai perso...');
+}
+
+function randomNumber() {
+    var number = Math.floor(Math.random() * 5) + 1;
+
+    return number;
+}
+
+function evenOdd(num1, num2) {
+    var result = num1 + num2;
+    if (result % 2 == 0) {
+        console.log('pari');
+        result = 'pari';
+    } else {
+        console.log('dispari');
+        result = 'dispari';
+    }
+
+    return result;
 }
